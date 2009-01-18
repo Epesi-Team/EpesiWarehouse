103e6a408ac16f851ebac568602ab090cac8536e
Arkadiusz Bisaga <ethnar86@gmail.com>
2009-01-18 16:46:03 +0000

    Warehosue - UPC length set to 128

--- Items/ItemsInstall.php
+++ Items/ItemsInstall.php
@@ -26,7 +26,7 @@ class Premium_Warehouse_ItemsInstall extends ModuleInstall {
 			array('name'=>'Item Type', 		'type'=>'select', 'param'=>'__COMMON__::Premium_Warehouse_Items_Type', 'filter'=>true, 'required'=>false, 'extra'=>false, 'visible'=>true),
 			array('name'=>'Item Name', 		'type'=>'text', 'required'=>true, 'param'=>'128', 'extra'=>false, 'visible'=>true,'display_callback'=>array('Premium_Warehouse_ItemsCommon', 'display_item_name')),
 			array('name'=>'Product Code', 	'type'=>'text', 'required'=>false, 'param'=>'32', 'extra'=>false, 'visible'=>false),
-			array('name'=>'UPC', 			'type'=>'text', 'required'=>false, 'param'=>'12', 'extra'=>false, 'visible'=>true),
+			array('name'=>'UPC', 			'type'=>'text', 'required'=>false, 'param'=>'128', 'extra'=>false, 'visible'=>true),
 			array('name'=>'Quantity on Hand','type'=>'integer', 'required'=>true, 'extra'=>false, 'visible'=>true),
 			array('name'=>'Quantity Sold',	'type'=>'calculated', 'required'=>false, 'extra'=>false, 'visible'=>true, 'display_callback'=>array('Premium_Warehouse_ItemsCommon', 'display_quantity_sold')),
 			array('name'=>'Reorder point', 	'type'=>'integer', 'required'=>true, 'extra'=>false, 'visible'=>false),
