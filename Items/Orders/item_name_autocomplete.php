cc14e3f527d318d7c253cc4fe2a2efe5ac9bd0b3
Arkadiusz Bisaga <ethnar86@gmail.com>
2009-04-21 13:56:06 +0000

    Warehouse - Orders bug fix

--- Items/Orders/item_name_autocomplete.php
+++ Items/Orders/item_name_autocomplete.php
@@ -24,7 +24,7 @@ $trans_type = $trans['transaction_type'];
 $qry = array();
 $vals = array();
 $words = explode(' ', $_POST['item_name']);
-if ($trans_type==1 && $trans['status']==2) {
+if ($trans_type==1 && $trans['status']==2 && $trans['warehouse']) {
 	$qry[] = 'EXISTS (SELECT id FROM premium_warehouse_location_data_1 AS pwl WHERE pwl.f_quantity!=0 AND pwl.f_item_sku=pwi.id AND pwl.f_warehouse=%d)';
 	$vals[] = $trans['warehouse'];
 }
