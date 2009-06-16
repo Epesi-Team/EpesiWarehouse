06e3a6e06d1fa8b277d9aea8e79d6a723c50df3d
Paweł Bukowski <pbukowski@telaxus.com>
2009-06-16 17:16:12 +0000

    bug fix

--- eCommerce/quickcart33pro/config/general.php
+++ eCommerce/quickcart33pro/config/general.php
@@ -2,7 +2,7 @@
 //error_reporting( E_ALL );
 unset( $config, $aMenuTypes, $aPhotoTypes, $lang, $aBanners, $aBoxes, $aUser );
 
-define('EPESI_DIR','../../../../../data/');
+define('EPESI_DATA_DIR','../../../../../data/');
 
 /*
 * Directories
