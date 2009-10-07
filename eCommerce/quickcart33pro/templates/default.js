b027c144e168ef7b1e9bc548ad69062fb5351cdb
Paweł Bukowski <pbukowski@telaxus.com>
2009-10-07 15:42:30 +0000

    ecommerce icecat bug fix

---
+++ eCommerce/quickcart33pro/templates/default.js
@@ -0,0 +1,8 @@
+function showLang() {
+	if(document.getElementById('lang').style.display == 'block') {
+		document.getElementById('lang').style.display = 'none';
+	}
+	else {
+		document.getElementById('lang').style.display = 'block';
+	}
+}
