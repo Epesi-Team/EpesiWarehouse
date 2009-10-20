ee28985656f73802b291144584844db12ade56b5
Arkadiusz Bisaga <ethnar86@gmail.com>
2009-10-20 10:40:33 +0000

    Sales Report - bug fix

--- SalesReport/SalesReportCommon_0.php
+++ SalesReport/SalesReportCommon_0.php
@@ -42,7 +42,53 @@ class Premium_Warehouse_SalesReportCommon extends ModuleCommon {
 		DB::Execute('DELETE FROM premium_warehouse_sales_report_purchase_lifo_tmp');
 		$purchases = DB::Execute('SELECT *, o.id AS order_id FROM premium_warehouse_items_orders_details_data_1 AS od LEFT JOIN premium_warehouse_items_orders_data_1 AS o ON o.id=od.f_transaction_id WHERE od.active=1 AND (o.f_transaction_type=0 OR o.f_transaction_type=4) AND o.f_status=20 ORDER BY o.f_transaction_date ASC, o.f_transaction_type ASC, o.created_on ASC');
 		$id = 0;
-		while ($trans = $purchases->FetchRow()) {
+		DB::Execute('DELETE FROM premium_warehouse_sales_report_earning');
+		$sales = DB::Execute('SELECT *, od.id AS od_id FROM premium_warehouse_items_orders_details_data_1 AS od LEFT JOIN premium_warehouse_items_orders_data_1 AS o ON o.id=od.f_transaction_id WHERE od.active=1 AND o.f_transaction_type=1 AND o.f_status=20 ORDER BY o.f_transaction_date ASC');
+		$sale = $sales->FetchRow();
+		do {
+			$trans = $purchases->FetchRow();
+			while ($sale && (!$trans || $trans['f_transaction_date']>$sale['f_transaction_date'])) {
+				$sale['f_price'] = Utils_CurrencyFieldCommon::get_values($sale['f_net_price']);
+				$net_price = $sale['f_price'][0];
+				if ($sale['f_price'][1]!=$currency)
+					$net_price = self::currency_exchange($net_price, $sale['f_price'][1], $currency);
+				$gross_price = round((100+Data_TaxRatesCommon::get_tax_rate($sale['f_tax_rate']))*$net_price/100, $prec);
+				$net_price *= $multip;
+				$gross_price *= $multip;
+	
+				$earnings = array('g_lifo'=>0, 'g_fifo'=>0, 'n_lifo'=>0, 'n_fifo'=>0);
+				
+				$sold_qty = $sale['f_quantity'];
+				$items = DB::Execute('SELECT * FROM premium_warehouse_sales_report_purchase_fifo_tmp WHERE item_id=%d AND warehouse=%d ORDER BY id ASC', array($sale['f_item_name'], $sale['f_warehouse']));
+				while ($sold_qty>0 && $item=$items->FetchRow()) {
+					$qty = min($sold_qty, $item['quantity']);
+					$sold_qty -= $qty;
+					$earnings['g_fifo'] += ($gross_price-$item['gross_price'])*$qty;
+					$earnings['n_fifo'] += ($net_price-$item['net_price'])*$qty;
+					if ($item['quantity']>$qty)
+						DB::Execute('UPDATE premium_warehouse_sales_report_purchase_fifo_tmp SET quantity=%d WHERE id=%d', array($item['quantity']-$qty, $item['id']));
+					else
+						DB::Execute('DELETE FROM premium_warehouse_sales_report_purchase_fifo_tmp WHERE id=%d', array($item['id']));
+				}
+				$qty_fifo = $sale['f_quantity']-$sold_qty;
+				$sold_qty = $sale['f_quantity'];
+				$items = DB::Execute('SELECT * FROM premium_warehouse_sales_report_purchase_lifo_tmp WHERE item_id=%d AND warehouse=%d ORDER BY id DESC', array($sale['f_item_name'], $sale['f_warehouse']));
+				while ($sold_qty>0 && $item=$items->FetchRow()) {
+					$qty = min($sold_qty, $item['quantity']);
+					$sold_qty -= $qty;
+					$earnings['g_lifo'] += ($gross_price-$item['gross_price'])*$qty;
+					$earnings['n_lifo'] += ($net_price-$item['net_price'])*$qty;
+					if ($item['quantity']>$qty)
+						DB::Execute('UPDATE premium_warehouse_sales_report_purchase_lifo_tmp SET quantity=%d WHERE id=%d', array($item['quantity']-$qty, $item['id']));
+					else
+						DB::Execute('DELETE FROM premium_warehouse_sales_report_purchase_lifo_tmp WHERE id=%d', array($item['id']));
+				}
+				$qty_lifo = $sale['f_quantity']-$sold_qty;
+				
+				DB::Execute('INSERT INTO premium_warehouse_sales_report_earning VALUES(%d,%d,%d,%d,%d,%d,%d)', array($sale['od_id'], $qty_lifo, $qty_fifo, $earnings['g_lifo'], $earnings['g_fifo'], $earnings['n_lifo'], $earnings['n_fifo']));
+				$sale = $sales->FetchRow();
+			}
+			if (!$trans) break;
 			if ($trans['f_transaction_type']==0) {
 				$trans['f_price'] = Utils_CurrencyFieldCommon::get_values($trans['f_net_price']);
 				$net_price = $trans['f_price'][0];
@@ -83,49 +129,7 @@ class Premium_Warehouse_SalesReportCommon extends ModuleCommon {
 					}
 				}
 			}
-		}
-		DB::Execute('DELETE FROM premium_warehouse_sales_report_earning');
-		$sales = DB::Execute('SELECT *, od.id AS od_id FROM premium_warehouse_items_orders_details_data_1 AS od LEFT JOIN premium_warehouse_items_orders_data_1 AS o ON o.id=od.f_transaction_id WHERE od.active=1 AND o.f_transaction_type=1 AND o.f_status=20 ORDER BY o.f_transaction_date ASC');
-		while ($trans = $sales->FetchRow()) {
-			$trans['f_price'] = Utils_CurrencyFieldCommon::get_values($trans['f_net_price']);
-			$net_price = $trans['f_price'][0];
-			if ($trans['f_price'][1]!=$currency)
-				$net_price = self::currency_exchange($net_price, $trans['f_price'][1], $currency);
-			$gross_price = round((100+Data_TaxRatesCommon::get_tax_rate($trans['f_tax_rate']))*$net_price/100, $prec);
-			$net_price *= $multip;
-			$gross_price *= $multip;
-
-			$earnings = array('g_lifo'=>0, 'g_fifo'=>0, 'n_lifo'=>0, 'n_fifo'=>0);
-			
-			$sold_qty = $trans['f_quantity'];
-			$items = DB::Execute('SELECT * FROM premium_warehouse_sales_report_purchase_fifo_tmp WHERE item_id=%d AND warehouse=%d ORDER BY id ASC', array($trans['f_item_name'], $trans['f_warehouse']));
-			while ($sold_qty>0 && $item=$items->FetchRow()) {
-				$qty = min($sold_qty, $item['quantity']);
-				$sold_qty -= $qty;
-				$earnings['g_fifo'] += ($gross_price-$item['gross_price'])*$qty;
-				$earnings['n_fifo'] += ($net_price-$item['net_price'])*$qty;
-				if ($item['quantity']>$qty)
-					DB::Execute('UPDATE premium_warehouse_sales_report_purchase_fifo_tmp SET quantity=%d WHERE id=%d', array($item['quantity']-$qty, $item['id']));
-				else
-					DB::Execute('DELETE FROM premium_warehouse_sales_report_purchase_fifo_tmp WHERE id=%d', array($item['id']));
-			}
-			$qty_fifo = $trans['f_quantity']-$sold_qty;
-			$sold_qty = $trans['f_quantity'];
-			$items = DB::Execute('SELECT * FROM premium_warehouse_sales_report_purchase_lifo_tmp WHERE item_id=%d AND warehouse=%d ORDER BY id DESC', array($trans['f_item_name'], $trans['f_warehouse']));
-			while ($sold_qty>0 && $item=$items->FetchRow()) {
-				$qty = min($sold_qty, $item['quantity']);
-				$sold_qty -= $qty;
-				$earnings['g_lifo'] += ($gross_price-$item['gross_price'])*$qty;
-				$earnings['n_lifo'] += ($net_price-$item['net_price'])*$qty;
-				if ($item['quantity']>$qty)
-					DB::Execute('UPDATE premium_warehouse_sales_report_purchase_lifo_tmp SET quantity=%d WHERE id=%d', array($item['quantity']-$qty, $item['id']));
-				else
-					DB::Execute('DELETE FROM premium_warehouse_sales_report_purchase_lifo_tmp WHERE id=%d', array($item['id']));
-			}
-			$qty_lifo = $trans['f_quantity']-$sold_qty;
-			
-			DB::Execute('INSERT INTO premium_warehouse_sales_report_earning VALUES(%d,%d,%d,%d,%d,%d,%d)', array($trans['od_id'], $qty_lifo, $qty_fifo, $earnings['g_lifo'], $earnings['g_fifo'], $earnings['n_lifo'], $earnings['n_fifo']));
-		}
+		} while (true);
 	}  
 }
 
