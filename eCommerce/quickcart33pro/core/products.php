2340d39702ec084a9758d30bece6a02431ab5847
Paweł Bukowski <pbukowski@telaxus.com>
2009-12-11 15:17:35 +0000

    bug fix

--- eCommerce/quickcart33pro/core/products.php
+++ eCommerce/quickcart33pro/core/products.php
@@ -93,7 +93,7 @@ class Products
 					LEFT JOIN premium_ecommerce_availability_labels_data_1 avl ON (pr.f_available=avl.f_availability AND avl.f_language="'.LANGUAGE.'" AND avl.active=1) 
 					LEFT JOIN premium_warehouse_location_data_1 loc ON (loc.f_item_sku=it.id AND loc.f_quantity>0 AND loc.active=1)
 					LEFT JOIN premium_warehouse_wholesale_items dist ON (dist.item_id=it.id AND dist.quantity>0 AND dist.price=(SELECT MIN(tmp.price) FROM premium_warehouse_wholesale_items tmp WHERE tmp.item_id=it.id))
-					 WHERE pr.f_publish=1 AND pr.active=1 '.($where?' AND ('.$where.')':'').' GROUP BY it.id ORDER BY pr.f_position'.($limit!==null?' LIMIT '.(int)$limit.($offset!==null?' OFFSET '.(int)$offset:''):''));
+					 WHERE pr.f_publish=1 AND pr.active=1 AND it.active=1 '.($where?' AND ('.$where.')':'').' GROUP BY it.id ORDER BY pr.f_position'.($limit!==null?' LIMIT '.(int)$limit.($offset!==null?' OFFSET '.(int)$offset:''):''));
 
         $taxes = DB::GetAssoc('SELECT id, f_percentage FROM data_tax_rates_data_1 WHERE active=1');
 	$autoprice = getVariable('ecommerce_autoprice');
