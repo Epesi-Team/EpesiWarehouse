e764f3c64110af1718b3418b998d6565e097d0f7
Paweł Bukowski <pbukowski@telaxus.com>
2009-12-18 19:21:49 +0000

    bug fix

--- eCommerce/quickcart33pro/core/orders.php
+++ eCommerce/quickcart33pro/core/orders.php
@@ -514,7 +514,7 @@ class Orders
 		foreach($shops as $sh) {
 			$aData = array();
 			$aData['iShop'] = $sh['id'];
-			$aData['sName'] = $sh['f_address_1'].($sh['f_address_2']?'<br />'.$sh['f_address_2']:'').'<br />'.$sh['f_city'];
+			$aData['sName'] = '<span>'.$sh['f_address_1'].($sh['f_address_2']?'<br />'.$sh['f_address_2']:'').'<br />'.$sh['f_city'].'</span>';
           		$oTpl->setVariables( 'aData', $aData );
 		        $content .= $oTpl->tbHtml( $sFile, 'ORDER_PICKUP_SHOP_LIST' );
           	}
