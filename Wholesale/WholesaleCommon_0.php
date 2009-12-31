8b8e0869239d9e103659a1be39103bfff77ee878
Paweł Bukowski <pbukowski@telaxus.com>
2009-12-31 10:19:44 +0000

    bug fix

--- Wholesale/WholesaleCommon_0.php
+++ Wholesale/WholesaleCommon_0.php
@@ -415,6 +415,7 @@ class Premium_Warehouse_WholesaleCommon extends ModuleCommon {
 	
 	public static function cron() {
 		$dists = Utils_RecordBrowserCommon::get_records('premium_warehouse_distributor',array('<last_update'=>date('Y-m-d 8:00:00',time()-3600*23)));
+		$ret = '';
 		foreach($dists as $dist) {
 			$plugin = self::get_plugin($dist['plugin']);
 			$params = $plugin->get_parameters();
@@ -430,11 +431,12 @@ class Premium_Warehouse_WholesaleCommon extends ModuleCommon {
 			@unlink($filename);
 			ob_end_clean();
 			if ($res===true) { 
-				print('updated '.$dist['name'].'<br>');
+				$ret .= 'updated '.$dist['name'].'<br>';
 				$time = time();
 				Utils_RecordBrowserCommon::update_record('premium_warehouse_distributor', $dist['id'], array('last_update'=>$time));
 			}
 		}
+		return $ret;
 	}
 }
 ?>
