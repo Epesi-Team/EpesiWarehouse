f95d4b7a73cda3fc13f9a24ade397ce3d340a675
Paweł Bukowski <pbukowski@telaxus.com>
2010-01-22 14:46:02 +0000

    bug fix

--- eCommerce/quickcart33pro/core/compare.php
+++ eCommerce/quickcart33pro/core/compare.php
@@ -66,7 +66,7 @@ function listProductsCompare( $sFile, $sBlock ){
       if(empty( $aData['sDescriptionFull'] ) && !empty( $aData['sDescriptionShort'] ) )
     	$aData['sDescriptionFull'] = $aData['sDescriptionShort'];
       else
-        $aData['sDescriptionFull'] = preg_replace( '/\|n\|/', '', $aData['sDescriptionFull'] );
+        $aData['sDescriptionFull'] = changeTxt( preg_replace( '/\|n\|/', '', $aData['sDescriptionFull'] ), 'nlNds' );;
 
       $aData['sFeatures'] = null;
       if( isset( $aFeaturesProducts[$aData['iProduct']] ) ){
