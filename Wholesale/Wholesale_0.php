09b4c9b904fcfd17a9df72b898c15efcc6c0cbe1
Paweł Bukowski <pbukowski@telaxus.com>
2010-05-31 12:47:02 +0000

    wholesale

--- Wholesale/Wholesale_0.php
+++ Wholesale/Wholesale_0.php
@@ -255,7 +255,7 @@ class Premium_Warehouse_Wholesale extends Module {
                             '</span>'.
                             '<span id="link_it_'.$row['id'].'_choice">'.
                                 '<a href="javascript:void(0);" onclick="$(\'link_it_'.$row['id'].'_form\').style.display=\'inline\';$(\'link_it_'.$row['id'].'_choice\').style.display=\'none\'"><img src="'.Base_ThemeCommon::get_template_file('Premium/Warehouse/Wholesale','link.png').'" border="0" /></a>'.
-                                '<a '.$lp->get_href(array($row['id'])).' onClick="$(\'add_item_name\').value=\''.escapeJS($row['distributor_item_name']).'\'"><img src="'.Base_ThemeCommon::get_template_file('Premium/Warehouse/Wholesale','add_item.png').'" border="0" /></a>'.
+                                '<a '.$lp->get_href(array($row['id'])).' onClick="setTimeout(\'$(\\\'add_item_name\\\').value=\\\''.escapeJS($row['distributor_item_name']).'\\\';\',100);"><img src="'.Base_ThemeCommon::get_template_file('Premium/Warehouse/Wholesale','add_item.png').'" border="0" /></a>'.
                             '</span>';
                 }
             }
