16fec34e075888ea2ff5dc73b282d66c1c55b212
Paweł Bukowski <pbukowski@telaxus.com>
2010-07-12 11:45:57 +0000

    service fix

--- Items/Orders/item_name_autocomplete.php
+++ Items/Orders/item_name_autocomplete.php
@@ -29,7 +29,7 @@ $qry = array();
 $vals = array();
 $words = explode(' ', $_POST['item_name']);
 if (($trans_type==1 || $trans_type==4) && $trans['status']>=2 && $trans['warehouse']) {
-	$qry[] = 'EXISTS (SELECT id FROM premium_warehouse_location_data_1 AS pwl WHERE pwl.f_quantity!=0 AND pwl.f_item_sku=pwi.id AND pwl.f_warehouse=%d)';
+	$qry[] = '(pwi.f_item_type>=2 OR EXISTS (SELECT id FROM premium_warehouse_location_data_1 AS pwl WHERE pwl.f_quantity!=0 AND pwl.f_item_sku=pwi.id AND pwl.f_warehouse=%d))';
 	$vals[] = $trans['warehouse'];
 }
 foreach ($words as $w) {
