b065fc6a23d6a1a4e8a82177bb5f5d1cfc198f4a
Paweł Bukowski <pbukowski@telaxus.com>
2011-06-01 09:50:28 +0000

    wholesa

---
+++ Wholesale/lang/en.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Translation file
+ * @package epesi-translations
+ * @subpackage en
+ */
+global $translations;
+$translations['Premium_Warehouse_Wholesale']['Distributor']='';
+$translations['Premium_Warehouse_Wholesale']['Distributor Code']='';
+$translations['Premium_Warehouse_Wholesale']['Price']='';
+$translations['Premium_Warehouse_Wholesale']['Quantity']='';
+$translations['Premium_Warehouse_Wholesale']['Quantity Details']='';
+$translations['Premium_Warehouse_Wholesale']['Last Update']='';
+$translations['Premium_Warehouse_Wholesale']['Status']='';
+$translations['Premium_Warehouse_Wholesale']['Item Name']='';
+$translations['Premium_Warehouse_Wholesale']['Distributor Category']='';
+$translations['Premium_Warehouse_Wholesale']['Manufacturer']='';
+$translations['Premium_Warehouse_Wholesale']['URL']='';
+$translations['Premium_Warehouse_Wholesale']['Login']='';
+$translations['Premium_Warehouse_Wholesale']['Password']='';
+$translations['Premium_Warehouse_Wholesale']['Items in file:']='';
+$translations['Premium_Warehouse_Wholesale']['Items scanned:']='';
+$translations['Premium_Warehouse_Wholesale']['Items available:']='';
+$translations['Premium_Warehouse_Wholesale']['Items found in the system:']='';
+$translations['Premium_Warehouse_Wholesale']['Items scanned in the past:']='';
+$translations['Premium_Warehouse_Wholesale']['New items:']='';
+$translations['Premium_Warehouse_Wholesale']['New categories:']='';
+$translations['Premium_Warehouse_Wholesale']['Unknown']='';
+$translations['Premium_Warehouse_Wholesale']['Scan a file']='';
+$translations['Premium_Warehouse_Wholesale']['File downloaded.']='';
+$translations['Premium_Warehouse_Wholesale']['Scanning...']='';
+$translations['Premium_Warehouse_Wholesale']['Scan complete.']='';
+$translations['Premium_Warehouse_Wholesale']['ID']='';
+$translations['Premium_Warehouse_Wholesale']['Items']='';
+$translations['Premium_Warehouse_Wholesale']['Distributors']='';
+$translations['Premium_Warehouse_Wholesale']['MPN']='';
+$translations['Premium_Warehouse_Wholesale']['UPC']='';
+$translations['Premium_Warehouse_Wholesale']['Item Type']='';
+$translations['Premium_Warehouse_Wholesale']['Product Code']='';
+$translations['Premium_Warehouse_Wholesale']['Manufacturer Part Number']='';
+$translations['Premium_Warehouse_Wholesale']['Weight']='';
+$translations['Premium_Warehouse_Wholesale']['Tax Rate']='';
+$translations['Premium_Warehouse_Wholesale']['eCommerce publish']='';
+$translations['Premium_Warehouse_Wholesale']['Available data']='';
+$translations['Premium_Warehouse_Wholesale']['Create new item']='';
+$translations['Premium_Warehouse_Wholesale']['Weight is required and should be numeric']='';
+?>
