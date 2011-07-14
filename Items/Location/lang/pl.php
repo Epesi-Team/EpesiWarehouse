acac81152e53f23ba3ef2a37b34a1d2328b395a8
Paweł Bukowski <pbukowski@telaxus.com>
2011-07-14 08:04:30 +0000

    polish tranlsation

---
+++ Items/Location/lang/pl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translation file
+ * @package epesi-translations
+ * @subpackage pl
+ */
+global $translations;
+$translations['Premium_Warehouse_Items_Location']['Quantity En Route:']='Ilość w Drodze';
+$translations['Premium_Warehouse_Items_Location']['Total']='Razem';
+$translations['Premium_Warehouse_Items_Location']['Available Qty:']='Ilość Dostępna';
+$translations['Premium_Warehouse_Items_Location']['Reserved Qty:']='Ilość Zarezerwowana';
+$translations['Premium_Warehouse_Items_Location']['In %s']='%s';
+$translations['Premium_Warehouse_Items_Location']['Quantity on hand:']='Ilość w Magazynach';
+$translations['Premium_Warehouse_Items_Location']['%s']='%s';
+$translations['Premium_Warehouse_Items_Location']['--']='';
+$translations['Utils_RecordBrowser:premium_warehouse_location']['Quantity']='Ilosc';
+$translations['Utils_RecordBrowser:premium_warehouse_location']['Warehouse']='Magazyn';
+?>
---
+++ eCommerce/lang/pl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translation file
+ * @package epesi-translations
+ * @subpackage pl
+ */
+global $translations;
+$translations['Premium_Warehouse_eCommerce']['eCommerce - %s']='';
+$translations['Premium_Warehouse_eCommerce']['Contact']='Kontakt';
+$translations['Premium_Warehouse_eCommerce']['Company']='Firma';
+$translations['Premium_Warehouse_eCommerce']['Phone']='Telefon';
+$translations['Premium_Warehouse_eCommerce']['Published']='Wystawiony w Sklepie Int.';
+$translations['Premium_Warehouse_eCommerce']['Assigned category']='Dodany do Kategorii';
+$translations['Premium_Warehouse_eCommerce']['Available in warehouse']='Dostępny w Magazynie';
+$translations['Premium_Warehouse_eCommerce']['eCommerce item doesn\'t exist.']='';
+?>
