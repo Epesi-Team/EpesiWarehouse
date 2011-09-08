d086443bf9e9eddcc29f93110d0a750d7976aaa5
Janusz Tylek <jtylek@telaxus.com>
2011-09-08 13:17:12 +0000

    orders fix

--- Items/Orders/Orders_0.php
+++ Items/Orders/Orders_0.php
@@ -214,18 +214,18 @@ class Premium_Warehouse_Items_Orders extends Module {
 		$cols['transaction_status'] = false;			
 		$cols['warehouse'] = false;			
 		$header_prop = array(
-			'item_name'=>array('width'=>25, 'wrapmode'=>'nowrap'),
+			'item_name'=>array('width'=>'100px', 'wrapmode'=>'nowrap'),
 			'description'=>array('width'=>70, 'wrapmode'=>'nowrap'),
 			'gross_total'=>array('width'=>'50px', 'wrapmode'=>'nowrap'),
 			'tax_value'=>array('width'=>'50px', 'wrapmode'=>'nowrap'),
 			'tax_rate'=>array('width'=>'50px', 'wrapmode'=>'nowrap'),
 			'sww'=>array('width'=>15, 'wrapmode'=>'nowrap'),
 			'net_total'=>array('width'=>'50px', 'wrapmode'=>'nowrap'),
-			'net_price'=>array('width'=>28, 'wrapmode'=>'nowrap'),
-			'gross_price'=>array('width'=>28, 'wrapmode'=>'nowrap'),
+			'net_price'=>array('width'=>'80px', 'wrapmode'=>'nowrap'),
+			'gross_price'=>array('width'=>'80px', 'wrapmode'=>'nowrap'),
 			'debit'=>array('width'=>20, 'wrapmode'=>'nowrap'),
 			'credit'=>array('width'=>20, 'wrapmode'=>'nowrap'),
-			'quantity'=>array('name'=>'Qty', 'width'=>'50px', 'wrapmode'=>'nowrap'),
+			'quantity'=>array('name'=>'Qty', 'width'=>'35px', 'wrapmode'=>'nowrap'),
 			'serial'=>array('width'=>40, 'wrapmode'=>'nowrap')
 		);
 		if ($arg['transaction_type']==0) {
