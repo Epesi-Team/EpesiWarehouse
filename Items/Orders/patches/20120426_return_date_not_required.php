805aa07c54d01352b8547bdf200baa92cc32f874
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-04-26 14:06:57 +0000

    Patch compatibility fix for Postgres

--- Items/Orders/patches/20120426_return_date_not_required.php
+++ Items/Orders/patches/20120426_return_date_not_required.php
@@ -1,4 +1,4 @@
 <?php
 Utils_RecordBrowserCommon::set_QFfield_callback('premium_warehouse_items_orders_details','Return Date','Premium_Warehouse_Items_OrdersCommon::QFfield_return_date');
-DB::Execute('UPDATE `premium_warehouse_items_orders_details_field`  SET required=0 WHERE field="Return Date"');
+DB::Execute('UPDATE premium_warehouse_items_orders_details_field SET required=0 WHERE field=%s', array('Return Date'));
 ?>
