f6ce3216afa1f68a1c015460bcbd035a2ee8f1bb
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-06-05 09:10:10 +0000

    Employee Sales Reports should require only admin clearance

--- EmpSalesReport/EmpSalesReport_0.php
+++ EmpSalesReport/EmpSalesReport_0.php
@@ -27,7 +27,7 @@ class Premium_Warehouse_EmpSalesReport extends Module {
 /************************************************************************************/
 	public function body() {
 
-		if (!Base_AclCommon::i_am_admin() || !Base_AclCommon::i_am_sa()) {
+		if (!Base_AclCommon::i_am_admin()) {
 		print($this->t('You don\'t have permission to access this module'));
 		return;
 		}
