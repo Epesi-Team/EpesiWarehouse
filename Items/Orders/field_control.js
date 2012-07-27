cf8c8a38e5098beb5aa1fec3257242886ad35b10
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-07-27 13:43:52 +0000

    Warehouse - minor bug fix

--- Items/Orders/field_control.js
+++ Items/Orders/field_control.js
@@ -15,10 +15,10 @@ warehouse_orders_hide_fields = function(trans_type, status, shipment_type, payme
 	warehouse_orders_hide_field('status', (warehouse_order_mode=='add'));
 	warehouse_orders_hide_field('company', (warehouse_order_mode=='view' || trans_type==2 || trans_type==4));
 	warehouse_orders_hide_field('contact', (warehouse_order_mode=='view' || trans_type==2 || trans_type==4));
-	var payments = ($('payment') && $('payment').checked) || warehouse_order_mode=='view'?true:false;
+	var payments = ($('payment') && $('payment').type=='checkbox' && !$('payment').checked)?false:true;
 	warehouse_orders_hide_field('payment_type', (!payments || trans_type==2 || trans_type==4));
 	warehouse_orders_hide_field('payment_no', ((status<2 && trans_type!=3) || payment_type==0 || !payments || trans_type==2 || trans_type==4));
-	warehouse_orders_hide_field('terms', ((status<2 && trans_type!=3) || !payments || trans_type==2 || trans_type==4));
+	warehouse_orders_hide_field('terms', (!payments || trans_type==2 || trans_type==4));
 	warehouse_orders_hide_field('total_value', (trans_type==2 || trans_type==4));
 	warehouse_orders_hide_field('invoice_number', (!payments || trans_type==4));
 	warehouse_orders_hide_field('invoice_print_date', (!payments || trans_type==4));
