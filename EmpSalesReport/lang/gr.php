2c30d4ddd2a4aa70d2c210a4e991730a9fbf6b23
Arkadiusz Bisaga <ethnar86@gmail.com>
2012-09-20 13:08:42 +0000

    Translations updated with automated tool

--- EmpSalesReport/lang/gr.php
+++ EmpSalesReport/lang/gr.php
@@ -8,7 +8,7 @@ global $translations;
 $translations['Total']='';
 $translations['Employee Sales']='';
 $translations['Inventory Management']='';
-$translations['You don\\\'t have permission to access this module']='';
+$translations['You don\'t have permission to access this module']='';
 $translations['Employee Sales Report, %s']='';
 $translations['Emp_Sales_Report_%s']='';
 $translations['Employee Sales Report']='';
