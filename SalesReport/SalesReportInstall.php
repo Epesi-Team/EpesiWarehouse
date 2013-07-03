9640bdc36eb6526f4b41a8d01532139027dc8bc2
Adam Bukowski <abukowski@telaxus.com>
2013-07-03 14:24:56 +0000

    Sales Report calculator, add storing sold quantity in related transactions - update installation file

--- SalesReport/SalesReportInstall.php
+++ SalesReport/SalesReportInstall.php
@@ -3,7 +3,7 @@
  * WARNING: This is a commercial software
  * Please see the included license.html file for more information
  *
- * @author Janusz Tylek <jtylek@telaxus.com>
+ * @author Janusz Tylek <jtylek@telaxus.com>, Adam Bukowski <abukowski@telaxus.com>
  * @copyright Copyright &copy; 2008, Telaxus LLC
  * @license Commercial
  * @version 1.0
@@ -13,7 +13,7 @@
 defined("_VALID_ACCESS") || die('Direct access forbidden');
 
 class Premium_Warehouse_SalesReportInstall extends ModuleInstall {
-    const version = '1.5.0';
+    const version = '1.5.3';
 
 	public function install() {
 		DB::CreateTable('premium_warehouse_sales_report_earning',
@@ -28,12 +28,14 @@ class Premium_Warehouse_SalesReportInstall extends ModuleInstall {
 		);
         DB::CreateTable('premium_warehouse_sales_report_related_order_details_fifo',
             'order_details_id_sold I4,' .
-            'order_details_id_bought I4',
+            'order_details_id_bought I4' .
+            'quantity I4',
             array('constraints' => '')
         );
         DB::CreateTable('premium_warehouse_sales_report_related_order_details_lifo',
             'order_details_id_sold I4,' .
-            'order_details_id_bought I4',
+            'order_details_id_bought I4' .
+            'quantity I4',
             array('constraints' => '')
         );
         DB::CreateTable('premium_warehouse_sales_report_purchase_fifo_tmp',
@@ -67,7 +69,7 @@ class Premium_Warehouse_SalesReportInstall extends ModuleInstall {
 		Variable::set('premium_warehouse_ex_currency', 1);
 		return true;
 	}
-	
+
 	public function uninstall() {
 		DB::DropTable('premium_warehouse_sales_report_exchange');
 		DB::DropTable('premium_warehouse_sales_report_earning');
@@ -75,29 +77,29 @@ class Premium_Warehouse_SalesReportInstall extends ModuleInstall {
 		DB::DropTable('premium_warehouse_sales_report_purchase_lifo_tmp');
 		return true;
 	}
-	
+
 	public function version() {
 		return array(self::version);
 	}
-	
+
 	public function requires($v) {
 		return array(
 			array('name'=>'Utils/RecordBrowser/Reports','version'=>0),
 			array('name'=>'Premium/Warehouse','version'=>0),
 			array('name'=>'Libs/OpenFlashChart','version'=>0));
 	}
-	
+
 	public static function info() {
 		return array(
 			'Description'=>'',
-			'Author'=>'Janusz Tylek <jtylek@telaxus.com>',
+			'Author'=>'Janusz Tylek <jtylek@telaxus.com>, Adam Bukowski <abukowski@telaxus.com>',
 			'License'=>'<a href="modules/Premium/Warehouse/SalesReport/license.html" TARGET="_blank">Commercial</a>');
 	}
-	
+
 	public static function simple_setup() {
         return array('package'=>__('Inventory Management'));
 	}
-	
+
 }
 
 ?>
