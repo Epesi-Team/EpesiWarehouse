423939dca4f06a4b67fa53680f637c74351048c8
Adam Bukowski <abukowski@telaxus.com>
2016-03-02 10:44:16 +0000

    Items orders - show description on Transaction History tab in Item

--- Items/Orders/Orders_0.php
+++ Items/Orders/Orders_0.php
@@ -275,7 +275,7 @@ class Premium_Warehouse_Items_Orders extends Module {
 	public function transaction_history_addon($arg){
 		// TODO: service?
 		$rb = $this->init_module('Utils/RecordBrowser','premium_warehouse_items_orders_details');
-		$order = array(array('item_name'=>$arg['id']), array('quantity_on_hand'=>false,'description'=>false)+('item_name'=='item_name'?array('item_name'=>false):array()), array('transaction_id'=>'DESC'));
+		$order = array(array('item_name'=>$arg['id']), array('quantity_on_hand'=>false)+('item_name'=='item_name'?array('item_name'=>false):array()), array('transaction_id'=>'DESC'));
 		$rb->set_button(false);
 		$rb->set_defaults(array('item_name'=>$arg['id']));
 		$rb->set_header_properties(array(
